a57bf8e0385e2a4ab686e00540a059f80d5e6320
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-19 15:56:37 +0100

    add variables selector for both inputs

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
-const inputKmEl = document.querySelector
+const inputKmEl = document.querySelector('.input-km')
+console.log(inputKmEl);
+const inputAgeEl = document.querySelector('.input-age')
+
+console.log(inputAgeEl);
 
 
 
@@ -15,20 +19,20 @@ const inputKmEl = document.querySelector
 // const questionAge = Number (prompt ('Scrivere la propria età in numeri [ es 30]'))
 // console.log(questionAge);
 
-const standardPrice = questionKm * 0.21;
-const discount20 = standardPrice * 0.20;
-const discount40 = standardPrice * 0.40;
+// const standardPrice = questionKm * 0.21;
+// const discount20 = standardPrice * 0.20;
+// const discount40 = standardPrice * 0.40;
 
 
-if (questionAge < 18){
-    standardPrice -= standardPrice * discount20; 
-    // alert (`Il costo del tuo biglietto è € ${discount20.toFixed(2)} - hai diritto ad un 20% di sconto` );
-} 
-else if (questionAge > 65) {
-    standardPrice -= standardPrice * discount40; 
-    // alert  (`Il costo del tuo biglietto è € ${discount40.toFixed(2)} - hai diritto ad un 40% di sconto`);
-} 
-else {
-    // alert (`Il costo del tuo biglietto è € ${standardPrice.toFixed(2)}`);
-}
+// if (questionAge < 18){
+//     standardPrice -= standardPrice * discount20; 
+//     // alert (`Il costo del tuo biglietto è € ${discount20.toFixed(2)} - hai diritto ad un 20% di sconto` );
+// } 
+// else if (questionAge > 65) {
+//     standardPrice -= standardPrice * discount40; 
+//     // alert  (`Il costo del tuo biglietto è € ${discount40.toFixed(2)} - hai diritto ad un 40% di sconto`);
+// } 
+// else {
+//     // alert (`Il costo del tuo biglietto è € ${standardPrice.toFixed(2)}`);
+// }
 
